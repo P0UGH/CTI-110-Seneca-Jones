9287027943bdc291aaf51ad46e094ee9bf459530
P0UGH <senecadjonesii@gmail.com>
2025-02-06 10:15:08 -0500

    Add files via upload

---
+++ P1LAB1_JonesSeneca.py
@@ -0,0 +1,13 @@
+# 2/4/2025
+# Seneca Jones
+# P1LAB1
+# Using input and print functions to display a name. 
+
+# Get user input for first name
+first_name = input("Enter your first name:")
+# Get userinput for last name
+last_name =  input("Enter your last name:")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110!")
+
+# jigglypuff wigglytuff igglybuff :^)
